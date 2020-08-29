98a9b17f2c08b8195eaeb3a21b26526ee581c587
John Asper <agentx1994@gmail.com>
2020-08-29 09:55:47 -0400

    remove more unecessary files

--- libc/string/memcpy.libk.d
+++
@@ -1,6 +0,0 @@
-libc/string/memcpy.libk.o: libc/string/memcpy.c libc/include/string.h \
- libc/include/sys/cdefs.h \
- /home/john/opt/cross/lib/gcc/i686-elf/10.2.0/include/stddef.h
-libc/include/string.h:
-libc/include/sys/cdefs.h:
-/home/john/opt/cross/lib/gcc/i686-elf/10.2.0/include/stddef.h:
